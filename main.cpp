afb987f6e6852ae9c539abfb468229e54ace0ff1
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2016-12-16 17:53:07 +0200

    Recently needed this, an array rotation..

--- main.cpp
+++ main.cpp
@@ -2,86 +2,49 @@
 
 using namespace std;
 
-struct node{
-    int data;
-    struct node* next;
-};
-
-//Function to remove duplicated from s unsorted linked list;
-//tis is a brute force algo because we se two loops
-//just for exercise
-
-void removeDuplicates(struct node* start){
-
-    struct node *ptr1, *ptr2, *dup;
-    ptr1 = start;
-
-    //pick elements one by one
-
-    while (ptr1 != NULL && ptr1->next != NULL ){
-        ptr2 = ptr1;
-
-        //compare the picked element with rest of the elements
-        while(ptr2->next != NULL){
-            //if duplicated, then deleet it
-            if (ptr1->data == ptr2->next->data){
-                //sequece of steps is important here
-                dup = ptr2->next;
-                ptr2->next = ptr2->next->next;
-                delete(dup);
-            } else{
-            //this is trickier
-                ptr2 = ptr2->next;
-            }
-        }
-        ptr1 = ptr1->next;
-    }
+void read ( int arr[ ], int& length ){
+cin >> length;
+for ( int i = 0; i < length; i++ )
+            cin >> arr[i];
 }
 
-//function to push aa node
-void push(struct node** head_ref, int new_data);
-//function to print nodes in a given linked list
-void printList(struct node *node);
+void reverse_ ( int arr[ ], int length, int left, int right ){
+if ( arr == NULL || length == 0 )
+    return;
+
+while ( left < right ){
+    swap ( arr[left], arr[right] );
+    left ++;
+    right --;
+}
+}
+
+void rotate_ ( int arr[ ], int length, int order ){
+if ( arr == NULL || length == 0 || order < 0 ){
+    cout << " Illegal argument! ";
+}
+    if ( order > length )
+    order = order % length;
+    //length of first part
+    int a = length - order ;
+    reverse_(arr, length, 0, a);
+    reverse_(arr, length, a+1, length-1);
+    reverse_(arr, length, 0, length-1);
+}
+
+void write ( int arr[ ], int length ){
+for ( int i = 0; i < length; i++ )
+    cout << arr[i] << " ";
+}
 
 int main()
 {
-    struct node* start = NULL;
-    push(&start, 10);
-    push(&start, 11);
-    push(&start, 12);
-    push(&start, 11);
-    push(&start, 11);
-    push(&start, 12);
-    push(&start, 10);
-
-
-    cout<<"This is the list before removing duplicates: \n";
-    printList(start);
-    removeDuplicates(start);
-    cout <<"\nAnd here is the linked list after removal:\n";
-    printList(start);
-
+    int arr[100], length;
+    read(arr, length);
+    cout << "Type the order: ";
+    int order;
+    cin >> order;
+    rotate_(arr, length, order);
+    write(arr, length);
     return 0;
 }
-
-void push(struct node**head_ref, int new_data){
-    //allocate node
-    struct node* new_node = new node();
-    //put in the data
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    *head_ref = new_node;
-}
-
-void printList(struct node* node){
-    while(node!=NULL){
-
-        cout << node->data <<" ";
-        node = node->next;
-    }
-
-}
-
-
-
-
