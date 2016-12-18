e9978a35163d260f5a8af5ddb62fa0765f0202ab
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2016-12-18 19:40:51 +0200

    compotes codeforces today's contest

--- main.cpp
+++ main.cpp
@@ -1,50 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
-void read ( int arr[ ], int& length ){
-cin >> length;
-for ( int i = 0; i < length; i++ )
-            cin >> arr[i];
-}
-
-void reverse_ ( int arr[ ], int length, int left, int right ){
-if ( arr == NULL || length == 0 )
-    return;
-
-while ( left < right ){
-    swap ( arr[left], arr[right] );
-    left ++;
-    right --;
-}
-}
-
-void rotate_ ( int arr[ ], int length, int order ){
-if ( arr == NULL || length == 0 || order < 0 ){
-    cout << " Illegal argument! ";
-}
-    if ( order > length )
-    order = order % length;
-    //length of first part
-    int a = length - order ;
-    reverse_(arr, length, 0, a);
-    reverse_(arr, length, a+1, length-1);
-    reverse_(arr, length, 0, length-1);
-}
-
-void write ( int arr[ ], int length ){
-for ( int i = 0; i < length; i++ )
-    cout << arr[i] << " ";
-}
 
 int main()
 {
-    int arr[100], length;
-    read(arr, length);
-    cout << "Type the order: ";
-    int order;
-    cin >> order;
-    rotate_(arr, length, order);
-    write(arr, length);
+    int a, b, c;
+    cin >> a >> b >> c;
+    int compotes = 0;
+    if ( a == 0 || b == 0 || c == 0 ){cout << 0;}
+    else{
+    while ( a > 0 ){
+        if ( b / a >= 2 && c / a >= 4 ){
+            compotes = a + 2*a + 4*a;
+            break;
+        } else { a -- ;}
+    }}
+    cout << compotes;
     return 0;
 }
