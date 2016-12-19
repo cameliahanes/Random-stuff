09d1b07890b5ba659afe4a495627f1db28fa1bff
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2016-12-19 09:12:29 +0200

    BitDefender LaboratoryWork

---
+++ lab2bitP1.asm
@@ -0,0 +1,43 @@
+[bits 32]
+%include "msdn_defs.inc"
+section .text 
+
+extern  _printf
+extern _exit
+extern CreateFileA
+global  _main 
+extern GetLastError
+
+
+_main: 
+		push dword NULL
+		push dword FILE_ATTRIBUTE_NORMAL
+		push dword OPEN_EXISTING
+		push dword NULL
+		push dword 0
+		push dword GENERIC_READ|GENERIC_WRITE
+		PUSH DWORD FileName
+		call CreateFileA
+		
+		cmp eax, INVALID_HANDLE_VALUE
+		je eroare
+
+        push    DWORD text 
+        call    _printf
+        add     esp, 8
+        push    0
+        call    _exit
+        ret 
+
+		eroare:
+		call GetLastError
+		push eax
+		push dword format
+		call _printf
+		
+section .data
+
+text:   db      'hello?',0 
+;FileName db "fisier.txt",0
+FileName db "C:\\Users\\Camelia\\Documents\\test\\fisier.txt.txt", 0
+format: db "Error: %d", 0 
